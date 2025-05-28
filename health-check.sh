4ab16db3ccb82cd14cf3c811fbb65e1e87e7d583
Elise-Brook Davis-Hirst <elise-brook.davis-hirst@york.ac.uk>
2025-05-28 13:42:30 +0100

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,24 +1,13 @@
-# In the original repository we'll just print the result of status checks,
-# without committing. This avoids generating several commits that would make
-# later upstream merges messy for anyone who forked us.
-commit=true
-origin=$(git remote get-url origin)
-if [[ $origin == *statsig-io/statuspage* ]]
-then
-  commit=false
-fi
-
 KEYSARRAY=()
 URLSARRAY=()
 
 urlsConfig="./urls.cfg"
 echo "Reading $urlsConfig"
-while read -r line
-do
+while read -r line; do
   echo "  $line"
   IFS='=' read -ra TOKENS <<< "$line"
-  KEYSARRAY+=(${TOKENS[0]})
-  URLSARRAY+=(${TOKENS[1]})
+  KEYSARRAY+=("${TOKENS[0]}")
+  URLSARRAY+=("${TOKENS[1]}")
 done < "$urlsConfig"
 
 echo "***********************"
@@ -26,44 +15,33 @@ echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
 
 mkdir -p logs
 
-for (( index=0; index < ${#KEYSARRAY[@]}; index++))
-do
+for (( index=0; index < ${#KEYSARRAY[@]}; index++ )); do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
   
+  # Remove carriage return characters (in case of Windows line endings)
   url="${url//$'\r'/}"
 
   echo "  $key=$url"
-  for i in 1 2 3 4; 
-  do
+  for i in {1..4}; do
     response=$(curl --write-out '%{http_code}' --silent --output /dev/null "$url")
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
+    if [[ "$response" -eq 200 || "$response" -eq 202 || "$response" -eq 301 || "$response" -eq 302 || "$response" -eq 307 ]]; then
       result="success"
+      break
     else
       result="failed"
     fi
-    if [ "$result" = "success" ]; then
-      break
-    fi
     sleep 5
   done
+
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]
-  then
-    echo $dateTime, $result >> "logs/${key}_report.log"
-    # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
-    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
-  else
-    echo "    $dateTime, $result"
-  fi
+  echo "$dateTime, $result" >> "logs/${key}_report.log"
+  # Keep only the last 2000 lines
+  tail -2000 "logs/${key}_report.log" > "logs/${key}_report.tmp" && mv "logs/${key}_report.tmp" "logs/${key}_report.log"
 done
 
-if [[ $commit == true ]]
-then
-  # does this need to be here at all or can it be an NPA?
-  git config --global user.name 'Elise-Brook Davis-Hirst'
-  git config --global user.email 'elise-brook.davis-hirst@york.ac.uk'
-  git add -A --force logs/
-  git commit -am '[Automated] Update Health Check Logs'
-  git push
-fi
+git config --global user.name 'ytubot'
+git config --global user.email 'ytu501@york.ac.uk'
+git add -A --force logs/
+git commit -am '[Automated] Update Health Check Logs'
+git push
